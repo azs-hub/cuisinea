3271a887473f458b37795d4041ec9a13819ee183
Giselle <sibaAnais@users.noreply.github.com>
2024-06-06 21:28:38 +0530

    TailWind and PrimeVue set up

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,38 @@
 import './assets/main.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
+
+import Ripple from 'primevue/ripple'
+import Button from 'primevue/button'
+import Menubar from 'primevue/menubar'
+import Dropdown from 'primevue/dropdown'
+import InputText from 'primevue/inputtext'
+import Avatar from 'primevue/avatar'
+import Tag from 'primevue/tag'
+import Chip from 'primevue/chip'
+import Panel from 'primevue/panel'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, { ripple: true })
+
+app.component('PvButton', Button)
+app.component('PvMenubar', Menubar)
+app.component('PvDropdown', Dropdown)
+app.component('PvInputText', InputText)
+app.component('PvAvatar', Avatar)
+app.component('PvTag', Tag)
+app.component('PvChip', Chip)
+app.component('PvPanel', Panel)
+
+app.directive('ripple', Ripple)
 
 app.mount('#app')
